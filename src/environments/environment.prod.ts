07ff20393d8247c711d6754fc34f3c5de3aef2dc
vladnavrosjuk <navrosjuk@gmail.com>
2022-04-12 12:26:12 +0300

    Firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAZbTiDNvxJvqVWZhWzkX4lacLyUKG25tE",
+    authDomain: "instagramnc-8110f.firebaseapp.com",
+    projectId: "instagramnc-8110f",
+    storageBucket: "instagramnc-8110f.appspot.com",
+    messagingSenderId: "1017434515733",
+    appId: "1:1017434515733:web:5776a52bd126a32b33e11c"
+  }
 };
